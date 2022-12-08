e8b704aea48d7989f6fbc98d0b2408438b44dfdf
mott4vitor <dailyslokoscontatoofc@gmail.com>
2022-12-08 15:13:22 -0300

    first commit

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Carregando...',
+              style: TextStyle(color: Theme.of(context).primaryTextTheme.headline6?.color),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
